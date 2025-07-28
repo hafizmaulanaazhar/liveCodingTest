4d135680aee9b2d9f4bb84fba818e2e2c8d63beb
simply Done <hapis1855@gmail.com>
2025-07-28 19:25:16 +0700

    hapus syntaks tambahan di view

--- resources/views/livewire/action-plan.blade.php
+++ resources/views/livewire/action-plan.blade.php
@@ -70,7 +70,6 @@
             radio.addEventListener('change', checkForm);
         });
         checkForm();
-        a
         document.addEventListener('formReset', function() {
             titleInput.value = '';
             radios.forEach(radio => radio.checked = false);
