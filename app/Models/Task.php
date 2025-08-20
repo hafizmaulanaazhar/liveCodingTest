bd5d38a1c09a4150f5da106f79efb4d38c9f8e0f
simply Done <hapis1855@gmail.com>
2025-08-20 14:07:35 +0700

    update model task baseQuery

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Task extends Model
 {
@@ -17,4 +18,20 @@ class Task extends Model
         'detail_agenda',
 
     ];
+
+    public static function baseQuery($filters = [])
+    {
+        return self::when(isset($filters['done']), function ($query) use ($filters) {
+            return $query->where('done', $filters['done']);
+        })
+            ->when(isset($filters['category']), function ($query) use ($filters) {
+                return $query->where('category', $filters['category']);
+            })
+            ->when(isset($filters['today']), function ($query) {
+                return $query->whereDate('due_date', Carbon::today());
+            })
+            ->when(isset($filters['next7days']), function ($query) {
+                return $query->whereBetween('due_date', [Carbon::tomorrow(), Carbon::today()->addDays(7)]);
+            });
+    }
 }
--- app/Queries/SidebarQuery.php
+++ app/Queries/SidebarQuery.php
@@ -3,48 +3,39 @@
 namespace App\Queries;
 
 use App\Models\Task;
-use Carbon\Carbon;
 
 class SidebarQuery
 {
     public static function countTodayTasks()
     {
-        return Task::whereDate('due_date', Carbon::today())
-            ->where('done', false)
-            ->count();
+        return Task::baseQuery(['today' => true, 'done' => false])->count();
     }
 
     public static function countNext7DaysTasks()
     {
-        return Task::whereBetween('due_date', [Carbon::tomorrow(), Carbon::today()->addDays(7)])
-            ->where('done', false)
-            ->count();
+        return Task::baseQuery(['next7days' => true, 'done' => false])->count();
     }
 
     public static function countMainJobTasks()
     {
-        return Task::where('category', 'Main Job')
-            ->where('done', false)
-            ->count();
+        return Task::baseQuery(['category' => 'Main Job', 'done' => false])->count();
     }
 
     public static function countSideJobTasks()
     {
-        return Task::where('category', 'Side Job')
-            ->where('done', false)
-            ->count();
+        return Task::baseQuery(['category' => 'Side Job', 'done' => false])->count();
     }
 
     public static function countDoneTasks()
     {
-        return Task::where('done', true)->count();
+        return Task::baseQuery(['done' => true])->count();
     }
 
     public static function getCategories()
     {
-        return Task::select('category')
+        return Task::baseQuery(['done' => false])
+            ->select('category')
             ->selectRaw('COUNT(*) as total')
-            ->where('done', false)
             ->whereNotNull('category')
             ->groupBy('category')
             ->get();
